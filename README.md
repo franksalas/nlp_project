b52dcdd5d77d600175de2b95e0b50b3beea73d22
franksalas <frank.salas@gmail.com>
2018-12-06 02:14:38 -0600

    project done

---
+++ README.md
@@ -0,0 +1,133 @@
+# NLP Project
+
+# Objectives
+
+## Analyze unstructured data 
+
+ • Transforming unstructured data into structured data via information extraction system. For example, convert investor memo (text format) to a well-defined structure (table format with pre-defined values), such as investor name, preference (geography, property type, investment size)
+ 
+
+## Requirements
+- Extract useful entities from unstructured text data
+- reshape to structure with predefined values
+    - name
+    - preference
+        - geography
+        - property type
+        - investment size
+
+
+ 
+### Sample Investor Memo Data
+- converted to JSON format to replicate API call
+- dummy data was added to complete name entity
+- Client A:
+    - company: Argos Group
+    - contact: Adam
+- Client B:
+    - Company: Blue Industries
+    - None
+- Client C:
+    - Company: Circle Inc
+    - Contact: Carol
+
+
+## Possible Solution
+Apply spaCy to unstructured text
+
+1. view raw text
+```
+I recently caught up with Adam of Argos Group and he indicated they are actively trying to grow their
+        portfolio in the US. They have been focused in NYC, but with his recent addition to the team, are
+        looking in Chicago and on the West Coast. Their main focus continues to be high-rise office in CBDs, but
+        are also considering urban multi-housing, preferably with a value-add component.
+```
+
+1. Extract entities with spacy
+![memo 1](/images/memo_1A.PNG "memo_1")
+
+2. Save entities in a format compatible for structure manipulation
+
+```JSON
+{'PERSON': ['Adam'],
+ 'ORG': ['Argos Group'],
+ 'GPE': ['US', 'Chicago'],
+ 'LOC': ['NYC', 'the West Coast'],
+ 'DATE': ['CBDs']}
+```
+
+## Problems w/ Solution
+- Domain-specific terms not available
+- multiple versions of the same word return errors
+- not enough data for training
+
+## Recommendations
+- Updating the Named Entity Recognizer
+- needs more data for correct NER training
+- train for domain-specific terms
+ - Real Estate & Financial Abbreviations
+
+```
+[ROC,REIT,MOB,"value-add",...]
+```
+- example https://github.com/explosion/spacy/blob/master/examples/training/train_ner.py
+
+
+## Small Solutions
+## Multiple versions of similar word
+
+Apply regular expressions
+
+```python
+multi_family = '(?i)mult*\w*\-?fam*\w*'  # mUlTyFamily
+text = 'This multifamily is not very good. my Multi-family is better. Not as good as my MULTIFAMILY'
+match = re.findall(multi_family,text)
+match
+# ['multifamily', 'Multi-family', 'MULTIFAMILY']
+```
+
+## Extract monetary values from text
+
+```python
+money = '\$?(?i)\d{1,3}[m|b]\w*'  # CREAM
+text = '''
+But you ain't a Saint, this ain't KumbaYe
+But you got hurt because you did cool by 'Ye
+You gave him 20m without blinkin'
+He gave you 20 minutes on stage,...
+'''
+match = re.findall(money,text)
+match
+# ['20m']
+```
+
+
+
+## Expand Domain specific acronyms
+### text
+
+````
+I will be based in New York, mainly tasked with sourcing equity and debt investments in high-profile
+        real estate assets in gateway markets with equity ticket $30M and up. My team and I will also look for JV
+        and M&A opportunities of established real estate companies and platforms.
+```
+
+### results
+```
+JV: Joint Venture
+M&A:  Mergers and Acquisitions
+```
+
+## Problem with Small Solutions
+
+## Regular expressions
+
+>... Some people, when confronted with a problem, think “I know,
+I'll use regular expressions.”  Now they have two problems.
+    - Jamie Zawinski
+    
+## Domain specific acronyms
+- Does not scale
+- topic modeling as possible choice
+
+    
---
+++ data.py
@@ -0,0 +1,110 @@
+
+
+clients = {
+    'client_A':{
+        'memo_1':'''I recently caught up with Adam of Argos Group and he indicated they are actively trying to grow their
+        portfolio in the US. They have been focused in NYC, but with his recent addition to the team, are
+        looking in Chicago and on the West Coast. Their main focus continues to be high-rise office in CBDs, but
+        are also considering urban multi-housing, preferably with a value-add component.''',
+        
+        'memo_2':'''I will be based in New York, mainly tasked with sourcing equity and debt investments in high-profile
+        real estate assets in gateway markets with equity ticket $30M and up. My team and I will also look for JV
+        and M&A opportunities of established real estate companies and platforms.''',
+        
+        'memo_3':'''They intend to add a large subterranean retail complex as well as reposition the building after the
+        major tenant moves out in 2 years. For the most part they are value-add to opportunistic driven. They
+        are focused on the following markets: NYC, Boston, DC, Chicago, SF, and LA. They will look at office, MH,
+        and retail. They are solving to mid-teen returns and they have no max or min on their investment size.''',
+        
+        'memo_4':'''They are seeking large office/residential/retail deals in the Tri-State region. Looking for low teen return
+        profile and 100% ownership (no operating partners). Typically looking for long term value and can
+        handle temporary non cash flowing periods to help generate value. Currently not using much, if any,
+        leverage on their deals. Global portfolio is 70mm SF, US portfolio is two NYC assets (room to grow).''',
+    },
+    'client_B':{
+        'memo_1':'''Retail Property Overview 
+        * Target investments are dominant centres that need work / capital improvements
+        * Really like outlet malls
+        * Own and like Paris high street''',
+        'memo_2':'''Industrial Property Overview
+        Blue Industries’s non-traded REIT is currently deploying $500 million/month into multifamily and industrial assets,
+        with a focus on assets producing an immediate, steady cash flow with low cost basis.
+        
+        Typical parameters for the non-traded REIT are as follows:
+        * B, B+ market (Atlanta, Baltimore, Phoenix)
+        * Prefer to enter a secondary market and use higher leverage
+        * 10-11% IRR with 60% leverage
+        * 7% cash-on-cash returns
+        * 5% net return
+        
+        Given Blue Industries’s current appetite for industrial product, they are open to a wide range of equity checks
+        across primary and secondary markets, and programmatic ventures with proven operators and
+        developers.
+        
+        Blue Industries’s opportunistic fund are far less active right now given the current pricing environment. Return
+        metrics in these funds are hard to hit with cost basis where it is for industrial.''',
+
+        'memo_3':'''Multi-Family Property Overview
+        * Locations: looking everywhere in the US, but less active in the middle of the country. Avoid
+        tertiary markets and low class property types
+        * Very deep appetite for MF - $5.5B in MF last year. More stable and more downside protection
+        * 3 Funds
+            * Fund #1
+                * Class B, suburban garden, 80-90’s vintage
+                * High growth markets
+                * Deal size: $250M+
+            * Fund #2  Core-plus vehicle
+                * Lower on the rick threshold, capped at 50%
+                * Institutional quality in gateway markets
+                * Completed two recaps in Seattle
+                * Deal size: $250M
+        * Fund #3
+        * Invests heavily in MF
+        * 7-8% cash on cash
+        * Looking for $100M+ deals
+        * Very flexible''',
+        
+        'memo_4':'''They are open to geographies and can do deals as small as $50mm.
+        * Industrial: hungry for more industrial and need size/quality. Have difficulty pricing cold storage.
+        * Healthcare/MOB/AL/IL: Hugely bullish as they continue to grow in the space.
+        * Hospitality: cautious on the national hotel front, but interested in NYC
+        * West Coast: consensus seems that as they continue buying and selling in early 2017, they will be
+        a net buyer on the west coast in 2017.''',
+    },
+    'client_C':{
+        'memo_1':'''I had a call with Carol at Circle Inc to discuss their investment criteria for office acquisitions. As many of you
+        know, they are an office owner that has been focused on coastal markets and Chicago. He indicated
+        that they are now having strategic discussions about entry into other markets such as Austin, Denver,
+        Charlotte, Nashville, etc. At this point it is only exploratory and they are not ready to pursue new deals
+        yet. They are going to focus on Austin first and have asked for our help educating them on the market.  
+        We are coordinating a meeting next month in Austin to give them an overview and market tour. I let
+        him know that we can help in other markets and will be ready to assist at the appropriate time.''',
+        
+        'memo_2':'''They would like to see all core and value-add multifamily and office opportunities in their target
+        markets as well as all development deals.
+        * New York
+        * Chicago
+        * Boston
+        * San Francisco
+        * Los Angeles
+        * Portland/Seattle
+        * Washington DC''',
+        
+        'memo_3':'''Circle Inc is aggressively looking to build their MH pipeline in LA via ground up development similar to what
+        they are doing in the Bay Area and on the East Coast. Will look at apartments or condos, and ok with
+        un-entitled sites as long as there is some income to limit downside. 150 units+ and product type
+        agnostic (high/mid rise). Targeting a low-mid 5% un-trended ROC.''',
+        
+        'memo_4':'''Circle Inc is very focused on the following markets (read: 90% of what they will do) Boston, NYC, DC, Chicago,
+        Seattle. They will do value add and new development of office and multi-family. He mentioned they are
+        mainly focused on development as they do not like buying at 4.0-4.5% caps (they are building to 5.75%
+        to 6.0% ROC).
+        
+        Multi-family-They really like the fundamentals of multi-family as they continue to see a migration into
+        the cities across the U.S. and a movement away from home ownership. They believe transportation is
+        key. They will continue to develop multi-family in urban locations.
+        Office- They feel good about the fundamentals in all of their key markets. They are very focused on
+        being in top tier locations with transportation and amenities. They are seeing a lot of major companies
+        consolidating their offices into one accessible and highly desirable location and paying up for it.''',
+    },
+}
